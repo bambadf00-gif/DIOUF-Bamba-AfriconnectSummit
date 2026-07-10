1ca5fcf0bb4907c5e213f251a04cacd1bbd0e8ba
Bamba <bamba.df00@gmail.com>
2026-07-10 22:31:48 +0000

    Message :js: ajout des filtrages dynamiques et des animations fade sur les pages

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // ===========================================
     // =============menu burger===================
     // ===========================================
-    
+
     const menuMobile = document.querySelector("#menuMobile");
     const burger = document.querySelector(".burger")
 
@@ -56,4 +56,77 @@ document.addEventListener("DOMContentLoaded", () => {
             behavior: "smooth"
         })
     })
+
+
+    //====================================================================================
+    // ==================================section fondut===================================
+    // ===================================================================================
+
+    const sectionFondu = document.querySelectorAll("section");
+
+    if (sectionFondu) {
+        const observateurSection = new IntersectionObserver((courSection) => {
+            for (let sec of courSection) {
+                if (sec.isIntersecting) {
+                    sec.target.classList.add("visbleSection");
+                    observateurSection.unobserve(sec.target);
+                } else {
+                    sec.target.classList.remove("visbleSection");
+                }
+            }
+        });
+
+        for (let sectionVue of sectionFondu) {
+            observateurSection.observe(sectionVue);
+        }
+    };
+    /*=====================================================
+    ============filtrage dynamique des Intervennant========
+    ===================================================== */
+    const filtre = document.querySelectorAll(".btnFiltre");
+    const articles = document.querySelectorAll(".sceIntervenantAnime");
+
+    if (filtre.length > 0) {
+
+        for (let art of articles) {
+            art.classList.add("visi");
+        }
+
+        for (let btn of filtre) {
+            btn.addEventListener("click", () => {
+                for (let art of articles) {
+                    if (btn.dataset.category === "tous") {
+                        art.classList.add("visi");
+                    } else if (btn.dataset.category === art.dataset.category) {
+                        art.classList.add("visi");
+                    } else {
+                        art.classList.remove("visi");
+                    }
+                }
+            })
+        }
+    }
+
+    /*=====================================================
+    ============filtrage dynamique Proogramme========
+    ===================================================== */
+
+    const btnProgramme = document.querySelectorAll(".btnProgramme")
+    const pros = document.querySelectorAll(".pro")
+
+    pros[0].classList.add("vi");
+
+    if (btnProgramme) {
+        for (let btn of btnProgramme) {
+            btn.addEventListener("click", () => {
+                for (let p of pros) {
+                    if (btn.dataset.pro === p.dataset.pro) {
+                        p.classList.add("vi")
+                    } else {
+                        p.classList.remove("vi")
+                    }
+                }
+            })
+        }
+    }
 })
