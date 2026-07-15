49504bee19cb7556901fdc8d0b34ef7bb0dab4e9
Bamba <bamba.df00@gmail.com>
2026-07-15 22:32:21 +0000

    Message : js : validation du formulaire de contact compteurs animés au scrollet compte à rebours en temps réel

--- js/main.js
+++ js/main.js
@@ -114,7 +114,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnProgramme = document.querySelectorAll(".btnProgramme")
     const pros = document.querySelectorAll(".pro")
 
-    pros[0].classList.add("vi");
+    if (pros.length > 0) {
+        pros[0].classList.add("vi");
+    }
 
     if (btnProgramme) {
         for (let btn of btnProgramme) {
@@ -128,5 +130,125 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
             })
         }
+    };
+
+
+
+    const formulaire = document.querySelector(".formulaire");
+
+    if (formulaire) {
+        const nomUser = document.querySelector("#nomUser");
+        const mailUser = document.querySelector("#mailUser");
+        const messageUser = document.querySelector("#message");
+        const telUser = document.querySelector("#telUser");
+
+        //Sélection des paragraphes de messages d'erreur CSS
+        const erreurNom = document.querySelector(".erreurNom");
+        const erreurMail = document.querySelector(".erreurMail");
+        const erreurMessage = document.querySelector(".erreurMessage");
+        const erreurTel = document.querySelector(".erreurTel");
+
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        const mesVal1 = document.querySelector(".mesVal1");
+
+
+        formulaire.addEventListener("submit", (e) => {
+            e.preventDefault();
+            console.log(formulaire);
+
+            if (nomUser.value.trim().length < 2) {
+                erreurNom.classList.add("erreurVisible");
+            } else {
+                erreurNom.classList.remove("erreurVisible");
+            }
+            if (messageUser.value.trim().length < 20) {
+                erreurMessage.classList.add("erreurVisible");
+            } else {
+                erreurMessage.classList.remove("erreurVisible");
+            }
+            if (telUser.value.trim().length < 8) {
+                erreurTel.classList.add("erreurVisible");
+            } else {
+                erreurTel.classList.remove("erreurVisible");
+            }
+
+            if (regex.test(mailUser.value.trim())) {
+                erreurMail.classList.remove("erreurVisible");
+            } else {
+                erreurMail.classList.add("erreurVisible");
+            }
+
+            const mesVal =
+                nomUser.value.trim().length >= 2 &&
+                telUser.value.trim().length >= 9 &&
+                messageUser.value.trim().length >= 20 &&
+                regex.test(mailUser.value.trim())
+
+            if (mesVal) {
+                mesVal1.classList.add("voixme"); // s'affiche immédiatement
+                setTimeout(() => {
+                    mesVal1.classList.remove("voixme"); // disparaît après 3s
+                }, 3000);
+                nomUser.value = "";
+                telUser.value = "";
+                mailUser.value = "";
+                messageUser.value = "";
+            }
+        })
+    };
+
+    const nbrInQ = document.querySelectorAll(".nbrIn");
+    // ===============================================
+    // ===============================================
+    const observateurChifre = new IntersectionObserver((chiffreVerifiez) => {
+        for (let chiffre of chiffreVerifiez) {
+            if (chiffre.isIntersecting) {
+
+                observateurChifre.unobserve(chiffre.target);
+
+                let cpt = 0;
+                let val = Number(chiffre.target.dataset.nbrI)
+                let pas = Math.ceil(val / 100);
+
+                const calcul = setInterval(() => {
+                    if (cpt < val) {
+                        cpt += pas;
+                    } else {
+                        clearInterval(calcul)
+                    }
+                    chiffre.target.textContent = cpt;
+                }, 20);
+            }
+        }
+    })
+    for (let prest of nbrInQ) {
+        observateurChifre.observe(prest);
     }
-})
+
+    // ===============================================
+    // ===============================================
+
+    const chiffres = document.querySelectorAll(".nbrspan1grid1");
+    const dateFinale = new Date("2026-10-15T10:00:00").getTime();
+
+    setInterval(function () {
+        const maintenant = new Date().getTime();
+        const distance = dateFinale - maintenant;
+
+        const jours = Math.floor(distance / 1000 / 60 / 60 / 24);
+        const heures = Math.floor(distance / 1000 / 60 / 60 % 24);
+        const minutes = Math.floor(distance / 1000 / 60 % 60);
+        const secondes = Math.floor(distance / 1000 % 60);
+
+        chiffres[0].textContent = jours;
+        chiffres[1].textContent = heures;
+        chiffres[2].textContent = minutes;
+        chiffres[3].textContent = secondes;
+
+    }, 1000);
+
+    // ===============================================
+    // ===============================================
+    document.getElementById("anneeActuelle").textContent = new Date().getFullYear();
+});
